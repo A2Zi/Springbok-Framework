4c4329d8880534b4568c86fda72752d789b1fa50
Cyril CRETTE <cyril.crette@a2zi.fr>
2013-09-23 15:01:06 +0200

    pullAndMerge à la fin du script

--- mergeFromUpstream.sh
+++ mergeFromUpstream.sh
@@ -1,5 +1,9 @@
+#!/bin/sh
+# récupère les modifs de l'upstream et les merge sur la branche a2zi
 git checkout master
 git fetch upstream
 git merge upstream/master
-sudo su www-data -c "php ../enhance_cli.php"
+cd .. && sudo su www-data -c "php enhance_cli.php"
+cd src
 git checkout a2zi
+./pullAndMerge.sh
